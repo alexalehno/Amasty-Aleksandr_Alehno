30edb7d0e47fbe89840a96da77c6f26f4281d342
alexalehno <alekswakeup@gmail.com>
2021-08-14 15:38:47 +0300

    task-3

--- Aleksandr/Block/index.php
+++ Aleksandr/Block/index.php
@@ -2,9 +2,36 @@
 
 namespace Amasty\Aleksandr\Block;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
 
 class Index extends Template
 {
+    /**
+     * @var ScopeConfigInterface
+     */
+
+    private $scopeConfig;
+
+    public function __construct(
+        Context              $context,
+        ScopeConfigInterface $scopeConfig
+    )
+
+    {
+        $this->scopeConfig = $scopeConfig;
+        parent::__construct($context);
+    }
+
+    public function greetingText()
+    {
+        return $this->scopeConfig->getValue('aleksandr_config/general/greeting_text');
+    }
+
+    public function defaultValue()
+    {
+        return $this->scopeConfig->getValue('aleksandr_config/general/default_value');
+    }
 
 }
